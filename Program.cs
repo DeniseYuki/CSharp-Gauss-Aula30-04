63925c2660d2bcbf021a21b967687fbb94095c9e
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-30 14:20:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Program.cs.Model;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var motor = new Motor();
+            //var motor2 = new Motor();
+            motor.modelo = "v8";
+            motor.cabecote = "tambemnao";
+            Console.WriteLine(motor.modelo);
+            Console.WriteLine(motor.cabecote);
+
+
+        }
+    }
+}
